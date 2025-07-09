eec77e70c755962336784dfe9e161ed149e8d64a
Karthik Peddavarpu <148485018+Karthik9492@users.noreply.github.com>
2025-07-09 17:29:21 -0500

    Update database.sql

--- Script/database.sql
+++ Script/database.sql
@@ -1,21 +1,13 @@
 /*
 This script creates a new SQL Server database called DataWarehouse. Here's what it does step by step:
-
 Checks if the DataWarehouse database already exists.
-
 If it does exist, the script drops it completely — which means all existing data and objects in the database will be
 permanently deleted.
-
 After dropping (or if it didn’t exist to begin with), it creates a fresh DataWarehouse database.
-
 Inside this database, it creates three schemas commonly used in data warehousing:
-
 bronze – for raw/staging data
-
 silver – for cleaned and transformed data
-
-gold – for analytics-ready, curated data
-	
+gold – for analytics-ready, curated data	
 WARNING:
     Running this script will drop the entire 'DataWarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
